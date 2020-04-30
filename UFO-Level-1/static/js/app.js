60794e2446f3a133e9143323fc819fc0db64c74f
Brett Chadwick <brett.r.chadwick@gmail.com>
2020-04-30 10:40:43 -0500

    updating js

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,2 +1,16 @@
 var tableData=data;
 
+var tbody=d3.select("tbody");
+
+function buildTable(tableData){
+    tbody.text("");
+    tableData.forEach((event) => {
+        var row=tbody.append("tr");
+        Object.entries(event).forEach(function([key,value]){
+            var cell=row.append("td");
+            cell.text(value);
+        })
+    })
+};
+
+buildTable(tableData);
