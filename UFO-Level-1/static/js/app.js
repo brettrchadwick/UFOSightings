50c77b4741afbc65698dd1a5e7784e614804c6f3
Brett Chadwick <brett.r.chadwick@gmail.com>
2020-05-02 09:34:59 -0500

    updating final files

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -2,9 +2,9 @@ var tableData=data;
 
 var tbody=d3.select("tbody");
 
-function buildTable(tableData){
+function buildTable(info){
     tbody.text("");
-    tableData.forEach((event) => {
+    info.forEach((event) => {
         var row=tbody.append("tr");
         Object.entries(event).forEach(function([key,value]){
             var cell=row.append("td");
@@ -14,3 +14,18 @@ function buildTable(tableData){
 };
 
 buildTable(tableData);
+
+var button=d3.select("#filter-btn");
+//https://stackoverflow.com/questions/31831651/javascript-filter-array-multiple-conditions
+
+function filterTable(){
+    d3.event.preventDefault();
+    var dateInput=d3.select("#datetime");
+    var dateValue=dateInput.property("value");
+    console.log(dateValue)
+    var filteredData=tableData.filter(instance => instance.datetime===dateValue)
+    buildTable(filteredData);
+}
+
+
+button.on("click", filterTable);
